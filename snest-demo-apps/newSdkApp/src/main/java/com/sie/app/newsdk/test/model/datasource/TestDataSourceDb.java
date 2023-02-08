3f03e1505e6b4ea5f10ac0911112e2692edebf7f
xiaomi <296629801@qq.com>
2023-02-08 11:38:15 +0800

    validate demo

--- snest-demo-apps/newSdkApp/src/main/java/com/sie/app/newsdk/test/model/datasource/TestDataSourceDb.java
+++ snest-demo-apps/newSdkApp/src/main/java/com/sie/app/newsdk/test/model/datasource/TestDataSourceDb.java
@@ -9,32 +9,14 @@ import com.sie.snest.sdk.annotation.validate.Validate;
 public class TestDataSourceDb extends BaseModel {
 
     @Validate.NotBlank(groups = TestDataSource.Db.class)
-    @Validate.Null.List(
-            value = {
-                    @Validate.Null(groups = TestDataSource.Api.class),
-                    @Validate.Null(groups = TestDataSource.Iiot.class)
-            }
-    )
     @Property(columnName = "host", displayName = "连接地址")
     private String host;
 
     @Validate.NotBlank(groups = TestDataSource.Db.class)
-    @Validate.Null.List(
-            value = {
-                    @Validate.Null(groups = TestDataSource.Api.class),
-                    @Validate.Null(groups = TestDataSource.Iiot.class)
-            }
-    )
     @Property(columnName = "user", displayName = "用户名")
     private String user;
 
     @Validate.NotBlank(groups = TestDataSource.Db.class)
-    @Validate.Null.List(
-            value = {
-                    @Validate.Null(groups = TestDataSource.Api.class),
-                    @Validate.Null(groups = TestDataSource.Iiot.class)
-            }
-    )
     @Property(columnName = "password", displayName = "密码")
     private String password;
 }
--- snest-demo-apps/newSdkApp/src/main/java/com/sie/app/newsdk/test/model/datasource/TestDataSourceIiot.java
+++ snest-demo-apps/newSdkApp/src/main/java/com/sie/app/newsdk/test/model/datasource/TestDataSourceIiot.java
@@ -9,11 +9,5 @@ import com.sie.snest.sdk.annotation.validate.Validate;
 public class TestDataSourceIiot extends BaseModel {
     @Property(columnName = "uri", displayName = "URI")
     @Validate.NotBlank(groups = TestDataSource.Iiot.class)
-    @Validate.Null.List(
-            value = {
-                    @Validate.Null(groups = TestDataSource.Db.class),
-                    @Validate.Null(groups = TestDataSource.Api.class)
-            }
-    )
     private String uri;
 }
