bc37c156cbee2c74bd11dfdb630e098f223e7e96
xiaomi <296629801@qq.com>
2023-03-09 14:42:29 +0800

    演示内容

--- snest-demo-apps/newSdkApp/src/main/java/com/sie/app/newsdk/test/model/dict/DictTest.java
+++ snest-demo-apps/newSdkApp/src/main/java/com/sie/app/newsdk/test/model/dict/DictTest.java
@@ -8,6 +8,6 @@ import com.sie.snest.sdk.annotation.meta.Property;
 @Model(name = "base_dict_test", displayName = "字典测试")
 public class DictTest extends BaseModel<DictTest> {
     @Property(displayName = "用户性别")
-    @Dict(typeCode = "userSex", defaultValue = "0", multiple = true)
+    @Dict(typeCode = "userSex", defaultValue = "0")
     private String test;
 }
