142937b34d1d471894ce9bc0fd06b9115c09a840
xiaomi <296629801@qq.com>
2023-03-13 18:35:54 +0800

    20230309

--- snest-demo-apps/newSdkApp/src/main/java/com/sie/app/newsdk/test/model/validate/ValidateTest.java
+++ snest-demo-apps/newSdkApp/src/main/java/com/sie/app/newsdk/test/model/validate/ValidateTest.java
@@ -1,5 +1,6 @@
 package com.sie.app.newsdk.test.model.validate;
 
+import com.sie.snest.sdk.BaseModel;
 import com.sie.snest.sdk.annotation.meta.Model;
 import com.sie.snest.sdk.annotation.validate.Validate;
 
@@ -7,7 +8,7 @@ import com.sie.snest.sdk.annotation.validate.Validate;
  * 校验demo
  */
 @Model
-public class ValidateTest {
+public class ValidateTest extends BaseModel<ValidateTest> {
 
     @Validate.Email(message = "邮箱有误")
     private String email;
