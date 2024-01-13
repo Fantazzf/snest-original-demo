9f6bd62c8c578ad12b37bd79a2740eecc83d55c1
cyh <2798213571@qq.com>
2024-01-13 16:05:58 +0800

    新建了“借书记录”和“还书记录”的模型类，BorrowRecord和ReturnRecord

---
+++ snest-demo-apps/school/src/main/java/com/sie/app/school/model/BorrowRecord.java
@@ -0,0 +1,20 @@
+package com.sie.app.school.model;
+
+import com.sie.snest.engine.model.Bool;
+import com.sie.snest.sdk.BaseModel;
+import com.sie.snest.sdk.annotation.meta.Model;
+import com.sie.snest.sdk.annotation.meta.Property;
+
+import java.util.Date;
+
+@Model(name = "borrow_record",displayName = "借书记录",isAutoLog = Bool.True)
+public class BorrowRecord extends BaseModel<BorrowRecord> {
+//    @Property(displayName = "读者")
+//    private Reader reader;
+
+    @Property(displayName = "借书日期")
+    private Date borrowDate;
+
+//    @Property(displayName = "所借图书信息")
+//    private Book borrowBook;
+}
