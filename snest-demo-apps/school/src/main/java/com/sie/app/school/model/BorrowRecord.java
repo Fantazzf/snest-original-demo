d2ff64882baad0e0a9d7a2c3dd2a28a1e75b8ba4
cyh <2798213571@qq.com>
2024-01-13 20:58:07 +0800

    创建了“借书记录”和“还书记录”的视图

--- snest-demo-apps/school/src/main/java/com/sie/app/school/model/BorrowRecord.java
+++ snest-demo-apps/school/src/main/java/com/sie/app/school/model/BorrowRecord.java
@@ -9,12 +9,12 @@ import java.util.Date;
 
 @Model(name = "borrow_record",displayName = "借书记录",isAutoLog = Bool.True)
 public class BorrowRecord extends BaseModel<BorrowRecord> {
-//    @Property(displayName = "读者")
-//    private Reader reader;
+    @Property(displayName = "读者")
+    private Reader reader;
 
     @Property(displayName = "借书日期")
     private Date borrowDate;
 
-//    @Property(displayName = "所借图书信息")
-//    private Book borrowBook;
+    @Property(displayName = "所借图书信息")
+    private Book borrowBook;
 }
--- snest-demo-apps/school/src/main/java/com/sie/app/school/model/ReturnRecord.java
+++ snest-demo-apps/school/src/main/java/com/sie/app/school/model/ReturnRecord.java
@@ -9,12 +9,15 @@ import java.util.Date;
 
 @Model(name = "return_record",description = "还书记录",isAutoLog = Bool.True)
 public class ReturnRecord extends BaseModel<ReturnRecord> {
-//    @Property(displayName = "读者")
-//    private Reader reader;
+    @Property(displayName = "读者")
+    private Reader reader;
 
     @Property(displayName = "还书日期")
     private Date date;
 
-//    @Property(displayName = "所借图书信息")
-//    private Book book;
+    @Property(displayName = "所借图书信息")
+    private Book book;
+
+    @Property(displayName = "关联借书记录")
+    private BorrowRecord borrowRecord;
 }
