3bcb421229f8ca7176ae01c7cf9840e1731aff55
lqy <1264857069@qq.com>
2024-01-13 16:53:09 +0800

    创建了Book和Reader的模型

---
+++ snest-demo-apps/school/src/main/java/com/sie/app/school/model/Book.java
@@ -0,0 +1,36 @@
+package com.sie.app.school.model;
+
+import com.sie.snest.engine.model.Bool;
+import com.sie.snest.sdk.BaseModel;
+import com.sie.snest.sdk.annotation.meta.Model;
+import com.sie.snest.sdk.annotation.meta.Property;
+import com.sie.snest.sdk.annotation.orm.Option;
+import com.sie.snest.sdk.annotation.orm.Selection;
+import org.checkerframework.checker.units.qual.A;
+
+import java.util.Date;
+
+@Model(name = "book", description = "图书", isAutoLog = Bool.True)
+public class Book extends BaseModel<Book> {
+
+    @Property(displayName = "图书编码")
+    private Integer bookID;
+    @Property(displayName = "图书名称")
+    private String bookName;
+
+    @Property(displayName = "图书类型")
+    private String bookType;
+
+    @Property(displayName = "作者")
+    private String author;
+
+    @Property(displayName = "出版日期")
+    private Date publishDate;
+
+    @Selection(values = {
+            @Option(label = "出借中", value = "2"),
+            @Option(label = "在馆", value = "1")
+    })
+    @Property(displayName = "图书状态")
+    private String bookStatus;
+}
