8d3b6fd09f07dba10a7921476c020dd4b1cf5668
lqy <1264857069@qq.com>
2024-01-14 09:50:03 +0800

    book和reader的视图

--- snest-demo-apps/school/src/main/java/com/sie/app/school/model/Book.java
+++ snest-demo-apps/school/src/main/java/com/sie/app/school/model/Book.java
@@ -2,6 +2,8 @@ package com.sie.app.school.model;
 
 import com.sie.snest.engine.model.Bool;
 import com.sie.snest.sdk.BaseModel;
+import com.sie.snest.sdk.annotation.Dict;
+import com.sie.snest.sdk.annotation.meta.MethodService;
 import com.sie.snest.sdk.annotation.meta.Model;
 import com.sie.snest.sdk.annotation.meta.Property;
 import com.sie.snest.sdk.annotation.orm.Option;
@@ -19,6 +21,7 @@ public class Book extends BaseModel<Book> {
     private String bookName;
 
     @Property(displayName = "图书类型")
+    @Dict(typeCode = "book_type")
     private String bookType;
 
     @Property(displayName = "作者")
@@ -33,4 +36,64 @@ public class Book extends BaseModel<Book> {
     })
     @Property(displayName = "图书状态")
     private String bookStatus;
+
+    public Book setBookID(Integer bookID) {
+        this.set("bookID", bookID);
+        return this;
+    }
+
+    public Integer getBookID() {
+        return getInt("bookID");
+    }
+
+    public Book setBookName(String bookName) {
+        this.set("bookName", bookName);
+        return this;
+    }
+
+    public String getBookName() {
+        return getStr("bookName");
+    }
+
+    public Book setBookType(String bookType) {
+        this.set("bookType", bookType);
+        return this;
+    }
+
+    public String getBookType() {
+        return getStr("bookType");
+    }
+
+    public Book setAuthor(String author) {
+        this.set("author", author);
+        return this;
+    }
+
+    public String getAuthor() {
+        return getStr("author");
+    }
+
+    public Book setPublishDate(Date publishDate) {
+        this.set("publishDate", publishDate);
+        return this;
+    }
+
+    public Date getPublishDate() {
+        return getDate("publishDate");
+    }
+
+    public Book setBookStatus(String bookStatus) {
+        this.set("bookStatus", bookStatus);
+        return this;
+    }
+
+    public String getBookStatus() {
+        return getStr("bookStatus");
+    }
+
+    @MethodService(name = "BookService", auth = "admin", description = "图书信息管理服务")
+    public String BookService(String service){
+        return "hello";
+    }
 }
+
